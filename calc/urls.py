3ad3143ba25e9679c9c05ce468e262fdf82e4465
triskyi <tresorbi29@gmail.com>
2024-03-06 10:22:10 +0300

    Our second commit  Changes to be committed: 	deleted:    calc/__init__.py 	deleted:    calc/__pycache__/__init__.cpython-311.pyc 	deleted:    calc/__pycache__/urls.cpython-311.pyc 	deleted:    calc/__pycache__/views.cpython-311.pyc 	deleted:    calc/admin.py 	deleted:    calc/apps.py 	deleted:    calc/migrations/__init__.py 	deleted:    calc/models.py 	deleted:    calc/tests.py 	deleted:    calc/urls.py 	deleted:    calc/views.py 	new file:   media/candidate_images/IMG_7168.jpg 	deleted:    templates/base.html 	deleted:    templates/dashboard.html 	deleted:    templates/homi.html 	deleted:    templates/result.html
     Changes not staged for commit:
    	deleted:    assets/scripty.js
    	deleted:    assets/styles.css
    	deleted:    assets/vote.png
    	modified:   busa/__pycache__/urls.cpython-311.pyc
    	modified:   busa/__pycache__/views.cpython-311.pyc
    	modified:   busa/urls.py
    	modified:   busa/views.py
    	modified:   static/styles.css
    	modified:   templates/home.html
    	deleted:    templates/index.html
    	modified:   templates/login.html
    	modified:   templates/profile.html
    	modified:   templates/register.html
    
     Untracked files:
    	media/candidate_images/1.png
    	media/candidate_images/Dark_Blue_Futuristic_Technology_Facebook_Cover_1.png
    	media/candidate_images/IMG_7183.jpg
    	media/candidate_images/beach_2.jpg
    	media/candidate_images/product.jpg
    	media/candidate_images/vierra.jpg

--- calc/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Use a unique name, e.g., 'home'
-    path('add', views.add, name='add'),
-]
