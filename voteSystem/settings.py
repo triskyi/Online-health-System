536e5810f59316f635a6a0c4e73bea3d709b266e
triskyi <tresorbi29@gmail.com>
2024-04-22 04:20:21 +0300

    Changes to be committed: 	new file:   build_files.sh 	new file:   vercel.json 	modified:   voteSystem/settings.py 	deleted:    voteSystem/vercel.json 	modified:   voteSystem/wsgi.py

--- voteSystem/settings.py
+++ voteSystem/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'  # Add a leading slash
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
