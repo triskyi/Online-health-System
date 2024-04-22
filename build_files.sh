62944f668ec85fab02a59870087bec682033e741
triskyi <tresorbi29@gmail.com>
2024-04-22 05:11:44 +0300

    modified:   build_files.sh 	modified:   vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /path/to/your/virtualenv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+
+# Run migrations (if applicable)
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
