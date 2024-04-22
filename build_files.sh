8e4e830217c8b0b2b4b617ba65f4bf7fa8fdc445
triskyi <tresorbi29@gmail.com>
2024-04-22 05:18:32 +0300

    modified:   build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Activate virtual environment
-source /path/to/your/virtualenv/bin/activate
+source C:/Users/User/Envs/votenv/Scripts/activate
+
+# Set up environment variables
+export PATH="$VIRTUAL_ENV/Scripts:$PATH"
 
 # Install dependencies
 pip install -r requirements.txt
